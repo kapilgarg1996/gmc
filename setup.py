5dfe037715199c96819187aa2aa132401069ba6c
Kapil Garg <kapilgarg1996@gmail.com>
2017-04-25 17:23:12 +0530

    Project Setup

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='GMC',
+    version=1.0.0.dev1,
+    url='https://github.com/kapilgarg1996/gmc',
+    author='Kapil GC',
+    author_email='kapilgarg1996@gmail.com',
+    description=('A genre based music classifier project '
+                 'using neural networks'),
+    license='MIT',
+    packages=find_packages(exclude=['docs', 'bin', 'tests*']),
+    scripts=['/bin/gmc.py'],
+    entry_points={'console_scripts': [
+        'gmc=gmc.core.handler:execute_from_command_line',
+    ]},
+    install_requires=['tensorflow', 'librosa'],
+    extras_require={},
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Multimedia :: Sound/Audio :: Analysis',
+    ],
+)
