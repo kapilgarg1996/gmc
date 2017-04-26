1d0647d8acd9d6fec1294ffa7cb78407e6326706
Kapil Garg <kapilgarg1996@gmail.com>
2017-04-26 22:00:44 +0530

    Added gmc-main command functionality
    load settings from a specified file path.
    Does not work for python style module path.

--- gmc/core/handler.py
+++ gmc/core/handler.py
@@ -1,2 +1,38 @@
+"""
+Command handler module for GMC
+"""
+
+import sys
+import importlib
+import os
+from gmc.conf import ENVIRONMENT_VARIABLE
+from gmc.conf import settings
+
 def execute_from_command_line(argv=None):
-	print('hello')
+	"""
+	try to load a module specified in the given path.
+	Set GMC_SETTINGS_MODULE environment variable
+	"""
+	argv = argv or sys.argv[:]
+	if(len(argv) != 2):
+		print('Incorrect Usage of gmc-main command')
+		return
+	try:
+		mod = importlib.import_module(argv[1])
+		os.environ.setdefault(ENVIRONMENT_VARIABLE, argv[1])
+	except ImportError:
+		if os.path.exists(argv[1]):
+			module_file = os.path.basename(argv[1])
+			module_path = os.path.abspath(argv[1])
+			module_dir = os.path.dirname(module_path)
+			sys.path.append(module_dir)
+			module_name = module_file.split('.py')[0]
+			os.environ.setdefault(ENVIRONMENT_VARIABLE, module_name)
+		else:
+			print('Incorrect format for settings.py path')
+	
+	try:
+		print("Dataset Directory set to '%s'" % settings.DATASET_DIR)
+		print("Results Directory set to '%s'" % settings.BRAIN_DIR)
+	except AttributeError:
+		print('Could not load settings')
