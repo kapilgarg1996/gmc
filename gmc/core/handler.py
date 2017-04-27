6f17991723c19af0b821294da37c00f9728ccead
Kapil Garg <kapilgarg1996@gmail.com>
2017-04-27 21:19:05 +0530

    Added quiet parameter to suppress output

--- gmc/core/handler.py
+++ gmc/core/handler.py
@@ -8,13 +8,13 @@ import os
 from gmc.conf import ENVIRONMENT_VARIABLE
 from gmc.conf import settings
 
-def execute_from_command_line(argv=None):
+def execute_from_command_line(argv=None, quiet=False):
 	"""
 	try to load a module specified in the given path.
 	Set GMC_SETTINGS_MODULE environment variable
 	"""
 	argv = argv or sys.argv[:]
-	if(len(argv) != 2):
+	if(len(argv) != 2) and not quiet:
 		print('Incorrect Usage of gmc-main command')
 		return
 
@@ -24,12 +24,14 @@ def execute_from_command_line(argv=None):
 		module_dir = os.path.dirname(module_path)
 		sys.path.append(module_dir)
 		module_name = module_file.split('.py')[0]
-		os.environ.setdefault(ENVIRONMENT_VARIABLE, module_name)
+		os.environ[ENVIRONMENT_VARIABLE] = module_name
 	else:
-		print('Incorrect format for settings.py path')
+		if not quiet:
+			print('Incorrect format for settings.py path')
 	
-	try:
-		print("Dataset Directory set to '%s'" % settings.DATASET_DIR)
-		print("Results Directory set to '%s'" % settings.BRAIN_DIR)
-	except AttributeError:
-		print('Could not load settings')
+	if not quiet:
+		try:
+			print("Dataset Directory set to '%s'" % settings.DATASET_DIR)
+			print("Results Directory set to '%s'" % settings.BRAIN_DIR)
+		except AttributeError:
+			print('Could not load settings')
