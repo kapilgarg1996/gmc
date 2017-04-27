0a07bceb18e9827a2fb73b82e7647e6bbd493dd3
Kapil Garg <kapilgarg1996@gmail.com>
2017-04-27 16:16:05 +0530

    Added test module to create dummy dataset before testing

---
+++ gmc/test/__init__.py
@@ -0,0 +1,45 @@
+import unittest
+import os
+import tempfile
+import shutil
+
+GENRE_DIRS = [
+	'blues', 'classical', 'country', 'disco', 'hiphop',
+	'jazz', 'metal', 'pop', 'reggae', 'rock'
+]
+
+class TestCase(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		cls.dataset_dir = os.path.realpath(os.path.join(
+			tempfile.gettempdir(),
+			cls.__name__,
+			'dummy_dataset',
+		))
+
+		cls.brain_dir = os.path.realpath(os.path.join(
+			tempfile.gettempdir(),
+			cls.__name__,
+			'dummy_brain',
+		))
+
+		if not os.path.exists(cls.dataset_dir):
+			os.makedirs(cls.dataset_dir)
+
+		if not os.path.exists(cls.brain_dir):
+			os.makedirs(cls.brain_dir)
+
+		#create music directories and files
+		for gen_dir in GENRE_DIRS:
+			dir_path = os.path.join(cls.dataset_dir, gen_dir)
+			if not os.path.exists(dir_path):
+				os.makedirs(dir_path)
+				for i in range(5):
+					with open(os.path.join(dir_path, '%s%d.wav'%(gen_dir, i)), 'w'):
+						pass
+
+	@classmethod
+	def tearDownClass(cls):
+		shutil.rmtree(cls.dataset_dir)
+		shutil.rmtree(cls.brain_dir)
