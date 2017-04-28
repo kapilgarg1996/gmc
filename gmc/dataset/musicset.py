e0a9667e3cf2c720a470790b668ffa8e6cda2d5a
Kapil Garg <kapilgarg1996@gmail.com>
2017-04-29 01:39:25 +0530

    Added MusicSet file loader

--- gmc/dataset/musicset.py
+++ gmc/dataset/musicset.py
@@ -2,13 +2,23 @@
 Class MusicSet holds the files and their respective
 genres
 """ 
+import os
 from gmc.conf import settings
 
 class MusicSet:
-
 	def __init__(self, force_load=False, genres=None):
 		self.dataset_dir = settings.DATASET_DIR
 		self.results_dir = settings.BRAIN_DIR
 		self.force_load = force_load
+		self.genres = genres or settings.GENRES
+		self.files = {}
 
-	def load_files()
+	def load_files(self):
+		for genre in self.genres:
+			genre_path = os.path.join(self.dataset_dir, genre)
+			if os.path.isdir(genre_path):
+				self.files[genre] = []
+				for f in os.listdir(genre_path):
+					file_path = os.path.join(genre_path, f)
+					if os.path.isfile(file_path) and f.endswith(".wav"):
+						self.files[genre].append(file_path)
---
+++ tests/test_dataset/test_file_loader.py
@@ -0,0 +1,27 @@
+import unittest
+import os
+from gmc.conf import settings
+from gmc.test import TestCase
+from gmc.dataset import musicset
+
+class TestDummyFiles(TestCase):
+	def setUp(self):
+		self.fileset = musicset.MusicSet()
+		self.fileset.load_files()
+
+	def test_genres_count(self):
+		self.assertEqual(len(self.fileset.files), 10)
+
+	def test_files_count_per_genre(self):
+		for genre in self.fileset.files:
+			self.assertEqual(len(self.fileset.files[genre]), 5)
+
+@unittest.skipIf(os.environ.get("DUMMY") == "TRUE",
+	"not necessary when real dataset not supplied")
+class TestReadFiles(unittest.TestCase):
+	def setUp(self):
+		self.fileset = musicset.MusicSet()
+		self.fileset.load_files()
+
+	def test_genres_count(self):
+		self.assertEqual(len(self.fileset.files), 10)
