daf3049cfe10f30a001218c16f73af4e859a6046
Kapil Garg <kapilgarg1996@gmail.com>
2017-04-28 23:34:06 +0530

    Moved temporary settings file creation to TestCase class

--- gmc/test/__init__.py
+++ gmc/test/__init__.py
@@ -9,6 +9,12 @@ class TestCase(unittest.TestCase):
 
 	@classmethod
 	def setUpClass(cls):
+		super(TestCase, cls).setUpClass()
+		cls._old_settings = {}
+
+		for setting, value in settings:
+			cls._old_settings[setting] = value
+
 		cls.dataset_dir = os.path.realpath(os.path.join(
 			tempfile.gettempdir(),
 			cls.__name__,
@@ -36,7 +42,26 @@ class TestCase(unittest.TestCase):
 					with open(os.path.join(dir_path, '%s%d.wav'%(gen_dir, i)), 'w'):
 						pass
 
+		cls.settings_file = os.path.realpath(os.path.join(
+			tempfile.gettempdir(),
+			cls.__name__,
+			'settings.py',
+		))
+
+		with open(cls.settings_file, 'w') as set_file:
+			set_file.write("DATASET_DIR = '%s'\n" % cls.dataset_dir)
+			set_file.write("BRAIN_DIR = '%s'" % cls.brain_dir)
+
+		settings.modify({
+			'DATASET_DIR' : cls.dataset_dir,
+			'BRAIN_DIR' : cls.brain_dir
+		})
+
 	@classmethod
 	def tearDownClass(cls):
+		os.remove(cls.settings_file)
 		shutil.rmtree(cls.dataset_dir)
 		shutil.rmtree(cls.brain_dir)
+
+		settings.modify(cls._old_settings)
+		super(TestCase, cls).tearDownClass()
--- tests/test_gmc_command/test_gmc_main.py
+++ tests/test_gmc_command/test_gmc_main.py
@@ -12,25 +12,6 @@ from gmc.conf import settings, ENVIRONMENT_VARIABLE
 from gmc.test import TestCase
 
 class TestGMCMain(TestCase):
-
-	@classmethod
-	def setUpClass(cls):
-		super(TestGMCMain, cls).setUpClass()
-		cls.settings_file = os.path.realpath(os.path.join(
-			tempfile.gettempdir(),
-			cls.__name__,
-			'settings.py',
-		))
-
-		with open(cls.settings_file, 'w') as set_file:
-			set_file.write("DATASET_DIR = '%s'\n" % cls.dataset_dir)
-			set_file.write("BRAIN_DIR = '%s'" % cls.brain_dir)
-
-	@classmethod
-	def tearDownClass(cls):
-		os.remove(cls.settings_file)
-		super(TestGMCMain ,cls).tearDownClass()
-
 	def test_gmc_main_settings(self):
 		old_cwd = os.getcwd()
 		os.chdir(os.path.dirname(self.settings_file))
@@ -39,7 +20,6 @@ class TestGMCMain(TestCase):
 			stdout=subprocess.PIPE, stderr=subprocess.PIPE,
 			universal_newlines=True,
 		).communicate()
-
 		os.chdir(old_cwd)
 		self.assertTrue(self.dataset_dir in out)
 		self.assertTrue(self.brain_dir in out)
