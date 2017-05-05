b62adee92999f441464c28fb17a9b31dc8a29155
Kapil Garg <kapilgarg1996@gmail.com>
2017-05-06 00:35:10 +0530

    Incorporated caching in MusicSet load_files method

--- gmc/dataset/musicset.py
+++ gmc/dataset/musicset.py
@@ -3,29 +3,39 @@ Class MusicSet holds the files and their respective
 genres
 """ 
 import os
-from gmc.conf import settings
+import pickle
+import shutil
 import numpy as np
+from gmc.conf import settings
+from gmc.core.cache import store
+
+RESULT_DIR = 'musicset'
 
 class MusicSet:
+    results_dir = os.path.join(settings.BRAIN_DIR, RESULT_DIR)
+    force_load = False
     def __init__(self, force_load=False, genres=None):
-        self.dataset_dir = settings.DATASET_DIR
-        self.results_dir = settings.BRAIN_DIR
+        if not os.path.isdir(self.results_dir):
+            os.mkdir(self.results_dir)
         self.force_load = force_load
         self.genres = genres or settings.GENRES
         self.files = {}
-        self.data = None
-        self.labels = None
+        self.train = None
+        self.test = None
         self.encoded_genres = {}
 
+    @store(os.path.join(results_dir, 'loaded_files.dat'), 'files', force=force_load)
     def load_files(self):
         for genre in self.genres:
-            genre_path = os.path.join(self.dataset_dir, genre)
+            genre_path = os.path.join(settings.DATASET_DIR, genre)
             if os.path.isdir(genre_path):
                 self.files[genre] = []
                 for f in os.listdir(genre_path):
                     file_path = os.path.join(genre_path, f)
                     if os.path.isfile(file_path) and f.endswith(".wav"):
                         self.files[genre].append(file_path)
+            else:
+                print('No Directory found for genre: '+genre)
 
     def one_hot_encode_genres(self):
         total_genres = len(self.genres)
@@ -34,3 +44,6 @@ class MusicSet:
             encoded = self.encoded_genres[genre] = np.zeros(total_genres)
             encoded[genre_class] = 1
             genre_class += 1
+
+    def destroy_results(self):
+        shutil.rmtree(self.results_dir)
--- tests/test_dataset/test_file_loader.py
+++ tests/test_dataset/test_file_loader.py
@@ -5,9 +5,11 @@ from gmc.test import TestCase
 from gmc.dataset import musicset
 
 class TestDummyFiles(TestCase):
-    def setUp(self):
-        self.fileset = musicset.MusicSet()
-        self.fileset.load_files()
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.fileset = musicset.MusicSet()
+        cls.fileset.load_files()
 
     def test_genres_count(self):
         self.assertEqual(len(self.fileset.files), 10)
@@ -15,6 +17,11 @@ class TestDummyFiles(TestCase):
     def test_files_count_per_genre(self):
         for genre in self.fileset.files:
             self.assertEqual(len(self.fileset.files[genre]), 5)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.fileset.destroy_results()
+        super().tearDownClass()
 
 @unittest.skipIf(os.environ.get("DUMMY") == "TRUE",
     "not necessary when real dataset not supplied")
