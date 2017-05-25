63e0aeb0631d6496907462b8e7687b55f6846892
Kapil Garg <kapilgarg1996@gmail.com>
2017-05-26 05:27:31 +0530

    Fixed #3: Made Beat feature vector length constant
    First 10 beat locations are selected along with tempo
    resulting in a feature vector of length 11

--- gmc/dataset/features.py
+++ gmc/dataset/features.py
@@ -4,6 +4,7 @@ Module to retrieve features from a single music file.
 import librosa
 import numpy as np
 import pywt
+import tensorflow as tf
 
 def mfcc(filename, **kwargs):
     """
@@ -13,7 +14,7 @@ def mfcc(filename, **kwargs):
     Instead of frame-wise calculations, use mean, 
     std, max and min of all features.
 
-    Returns complete feature vector
+    Returns complete feature vector (160 features)
     """
     y, sr = librosa.load(filename)
     kwargs.setdefault('n_fft', int(sr*0.025))
@@ -42,23 +43,85 @@ def mfcc(filename, **kwargs):
 
 def dwt(filename, **kwargs):
     """
-    Compute wavelet coefficients
+    Compute wavelet coefficients frame wise and return computed features
+    (112 features)
     """
     y, sr = librosa.load(filename)
+    options = {}
+    options['frame_length'] = int(sr*0.25)
+    options['hop_length'] = int(sr*0.05)
+    frames = librosa.util.frame(y, **options)
+    frames = frames.T
+    wavelets = None
     kwargs.setdefault('level', 6)
-    dwt_coeffs = pywt.wavedec(y, 'db4', level=int(kwargs['level']))
-    mean_features = tuple(np.mean(coeffs, axis=-1) for coeffs in dwt_coeffs)
-    std_features =  tuple(np.std(coeffs, axis=-1) for coeffs in dwt_coeffs)
-    max_features = tuple(np.amax(coeffs, axis=-1) for coeffs in dwt_coeffs)
-    min_features = tuple(np.amin(coeffs, axis=-1) for coeffs in dwt_coeffs)
+    
+    for frame in frames:
+        dwt_coeffs = pywt.wavedec(frame, 'db4', level=int(kwargs['level']))
+        mean_features = tuple(np.mean(coeffs, axis=-1) for coeffs in dwt_coeffs)
+        std_features =  tuple(np.std(coeffs, axis=-1) for coeffs in dwt_coeffs)
+        max_features = tuple(np.amax(coeffs, axis=-1) for coeffs in dwt_coeffs)
+        min_features = tuple(np.amin(coeffs, axis=-1) for coeffs in dwt_coeffs)
 
-    all_features = mean_features + std_features + max_features + min_features
-    final_features = np.asarray(all_features)
-    return final_features
+        all_features = mean_features + std_features + max_features + min_features
+        final_features = np.asarray(all_features)
+
+        if wavelets is None:
+            wavelets = np.array(final_features)
+        else:
+            wavelets = np.vstack((wavelets, final_features,))
+
+    all_features = wavelets.T
+    mean_features = np.mean(all_features, axis=-1)
+    std_features = np.std(all_features, axis=-1)
+    max_features = np.amax(all_features, axis=-1)
+    min_features = np.amin(all_features, axis=-1)
+
+    final_features = np.vstack((mean_features, std_features, 
+        max_features, min_features,))
+
+    return np.ravel(final_features)
 
 def beat(filename, **kwargs):
+    """
+    Compute beats locations and find a relation among those locations.
+    Use a simple linear regressor to find W and b for beat locations
+    Return a feature vector (11 features)
+    """
     y, sr = librosa.load(filename)
     kwargs.setdefault('hop_length', 512)
     t, beats = librosa.beat.beat_track(y=y, sr=sr, hop_length=int(kwargs['hop_length']))
 
-    return np.hstack((t, beats,))
+    # rng = np.random
+    # train_X = np.arange(1,len(beats)+1)
+    # train_Y = beats
+    # n_samples = train_X.shape[0]
+
+    # # tf Graph Input
+    # X = tf.placeholder("float")
+    # Y = tf.placeholder("float")
+
+    # # Set model weights
+    # W = tf.Variable(rng.randn(), name="weight")
+    # b = tf.Variable(rng.randn(), name="bias")
+
+    # # Construct a linear model
+    # pred = tf.add(tf.multiply(X, W), b)
+
+    # # Mean squared error
+    # cost = tf.reduce_sum(tf.pow(pred-Y, 2))/(2*n_samples)
+    # # Gradient descent
+    # #  Note, minimize() knows to modify W and b because Variable objects are trainable=True by default
+    # optimizer = tf.train.GradientDescentOptimizer(0.02).minimize(cost)
+
+    # # Initializing the variables
+    # init = tf.global_variables_initializer()
+    # with tf.Session() as sess:
+    #     sess.run(init)
+    #     for epoch in range(50):
+    #         for (x, y) in zip(train_X, train_Y):
+    #             sess.run(optimizer, feed_dict={X: x, Y: y})
+
+    # features = np.array([t, W, b])
+    first_frames = beats[:10]
+
+    return np.hstack((t, first_frames,))
--- tests/test_dataset/test_features.py
+++ tests/test_dataset/test_features.py
@@ -4,14 +4,7 @@ from gmc.conf import settings
 from gmc.test import TestCase
 from gmc.dataset import musicset, features
 
-@unittest.skipIf(os.environ.get("DUMMY") == "TRUE",
-    "not necessary when real dataset not supplied")
 class TestTempFiles(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.fileset = musicset.MusicSet()
-        cls.fileset.load_files()
-
     def test_feature_computation(self):
         module_path = os.path.dirname(os.path.abspath(__file__))
         test_file = os.path.join(module_path, 'blues.00099.au.wav')
@@ -22,28 +15,24 @@ class TestTempFiles(unittest.TestCase):
         except Exception as e:
             self.fail(e)
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.fileset.destroy_results()
-
 @unittest.skipIf(os.environ.get("DUMMY") == "TRUE",
     "not necessary when real dataset not supplied")
 class TestRealFeatures(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.fileset = musicset.MusicSet(dirname='features')
+        cls.fileset = musicset.MusicSet(dirname='features2')
         cls.fileset.load_files()
 
     def test_train_data_loading(self):
         train = self.fileset.load_train_data()
         self.assertEqual(train.music.shape[0], 700)
-        self.assertEqual(train.music.shape[1], 188)
+        self.assertEqual(train.music.shape[1], 283)
         self.assertEqual(train.labels.shape[0], 700)
         self.assertEqual(train.labels.shape[1], 10)
 
     def test_test_data_loading(self):
         test = self.fileset.load_test_data()
         self.assertEqual(test.music.shape[0], 300)
-        self.assertEqual(test.music.shape[1], 188)
+        self.assertEqual(test.music.shape[1], 283)
         self.assertEqual(test.labels.shape[0], 300)
         self.assertEqual(test.labels.shape[1], 10)
