82f1423f8b28e4c964e465544b591f0bbf2e122d
Kapil Garg <kapilgarg1996@gmail.com>
2017-05-31 05:19:22 +0530

    Added accuracy and loss monitoring

--- gmc/core/models/nn.py
+++ gmc/core/models/nn.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import os
 from gmc.conf import settings
+from gmc.core.cache import store
 
 class NN:
     def __init__(self, dataset, n_input=None):
@@ -18,12 +19,12 @@ class NN:
         if not os.path.isdir(self.results_dir):
             os.mkdir(self.results_dir)
 
-    def train(self, display_step=100):
+    def train(self, display_step=100, out=False, path='model.final'):
         n_classes = len(settings.GENRES)
         n_input = self.n_input
-        x = self.x = tf.placeholder("float", [None, n_input])
-        y = self.y = tf.placeholder("float", [None, n_classes])
-        keep_prob = self.keep_prob = tf.placeholder(tf.float32)
+        x = self.x = tf.placeholder("float", [None, n_input], name='x')
+        y = self.y = tf.placeholder("float", [None, n_classes], name='y')
+        keep_prob = self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
 
         w = self.get_weights(n_input, n_classes)
         b = self.get_bias(n_classes)
@@ -34,6 +35,11 @@ class NN:
         saver = tf.train.Saver()
         self.correct_pred = correct_pred = tf.equal(tf.argmax(self.y_, 1), tf.argmax(self.y, 1))
         self.accuracy = accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+        train_acc = []
+        #val_acc = []
+        test_acc = []
+        train_cost = []
+        test_cost = []
         with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)) as sess:
             sess.run(init)
             step = 1
@@ -42,21 +48,44 @@ class NN:
                 sess.run(optimizer, feed_dict={x: bx, y:by, keep_prob:settings.NN['DROPOUT_PROB']})
                 if step % display_step == 0:
                     # Calculate batch accuracy
-                    acc = sess.run(accuracy, feed_dict={x: bx, y: by, keep_prob: 1.})
+                    acc = sess.run(accuracy, feed_dict={x: bx, 
+                        y: by, keep_prob: 1.})
                     # Calculate batch loss
-                    loss = sess.run(cost, feed_dict={x: bx, y: by, keep_prob: 1.})
-                    print("Iter " + str(step * settings.NN['BATCH_SIZE']) + ", Minibatch Loss= " + \
-                          "{:.6f}".format(loss) + ", Training Accuracy= " + "{:.5f}".format(acc))
+                    loss = sess.run(cost, feed_dict={x: bx, 
+                        y: by, keep_prob: 1.})
 
+                    train_acc.append(acc)
+
+                    #vac = sess.run(accuracy, 
+                    #    feed_dict={x: self.data.validation.music, y: self.data.validation.labels, keep_prob: 1.})
+                    tac = sess.run(accuracy, 
+                        feed_dict={x: self.data.test.music, y: self.data.test.labels, keep_prob: 1.})
+
+                    te_loss = sess.run(cost, 
+                        feed_dict={x: self.data.test.music, y: self.data.test.labels, keep_prob: 1.})
+
+                    train_cost.append(loss)
+                    test_cost.append(te_loss)
+                    #val_acc.append(vac)
+                    test_acc.append(tac)
                     save_path = saver.save(sess, os.path.join(self.results_dir, "model.ckpt"))
-                    print("Model saved in file: %s" % save_path)
+                    if out:
+                        print("Iter " + str(step * settings.NN['BATCH_SIZE']) + ", Minibatch Loss= " + \
+                              "{:.6f}".format(loss) + ", Training Accuracy= " + "{:.5f}".format(acc))
+
+                        #print("Validation Accuracy:", vac)
+                        print("Testing Accuracy:", tac)
+                        print("Model saved in file: %s" % save_path)
                 step += 1
-            save_path = saver.save(sess, os.path.join(self.results_dir, "model.final"))
+            save_path = saver.save(sess, os.path.join(self.results_dir, path))
+            storage = store(self.results_dir)
+            storage['save.path'] = save_path
             print("Model saved in file: %s" % save_path)
 
             print("Testing Accuracy:", sess.run(self.accuracy, 
                 feed_dict={x: self.data.test.music, y: self.data.test.labels, keep_prob: 1.}))
 
+        return train_acc, test_acc, train_cost, test_cost
 
     def prepare_layers(self):
         prev_layer = self.x
@@ -66,11 +95,11 @@ class NN:
             layer = tf.add(tf.matmul(prev_layer, w[i]), b[i])
             layer = tf.nn.relu(layer)
             self.layers.append(layer)
-            prev_layer = layer
+            prev_layer = self.layers[-1]
 
         drop_layer = tf.nn.dropout(self.layers[-1], self.keep_prob)
         self.layers.append(drop_layer)
-        out_layer = tf.matmul(self.layers[-1], w[-1]) + b[-1]
+        out_layer = tf.add(tf.matmul(self.layers[-1], w[-1]), b[-1], name='y_')
         self.layers.append(out_layer)
         return out_layer
 
